3cca59accd2fb3aefcd3e6072b3f9de0b9900a2e
CY2365 <ckyz1007@gmail.com>
2022-04-26 18:30:22 +0300

    Initial Commit

---
+++ Time Log/Time_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_LogApp.swift
+//  Time Log
+//
+//  Created by Cenk Yavuz on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Time_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
