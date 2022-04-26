880049334e0fa9f073f4aecf15d84f8ab9a82653
CY2365 <ckyz1007@gmail.com>
2022-04-26 21:55:38 +0300

    Add layout for calculator.

--- Time Log/ContentView.swift
+++ Time Log/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+let testArray: [String] = ["1", "2", "3", "Clear", "4", "5", "6", "-", "7", "8", "9", "+", "0", "="]
+var currentIndex = 0
+var rows: [GridItem] = [GridItem(.adaptive(minimum: 50))]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Button("1"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("2"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("3"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("Clear"){}.frame(maxWidth: .infinity, maxHeight: 50)
+            }
+            HStack {
+                Button("4"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("5"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("6"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("-"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                    
+            }
+            HStack {
+                Button("7"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("8"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("9"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("+"){}.frame(maxWidth: .infinity, maxHeight: 50)
+            }
+            HStack(alignment: .firstTextBaseline) {
+                Button("0"){}.frame(maxWidth: .infinity, maxHeight: 50)
+                Button("="){}.frame(maxWidth: .infinity, maxHeight: 50)
+            }
+        }
     }
 }
 
